5d4fd0e5869ab0924ba83b867ba8b3282f16aa10
vincentowler <vincentowler@outlook.com>
2022-09-26 14:50:06 -0400

    Enhance sizing logic

--- src/shared/Heading.tsx
+++ src/shared/Heading.tsx
@@ -3,7 +3,18 @@ type HeadingProps = {
   level: 1 | 2 | 3 | 4 | 5 | 6;
 };
 
+const sizeMap = {
+  1: "text-4xl",
+  2: "text-3xl",
+  3: "text-2xl",
+  4: "text-xl",
+  5: "text-lg",
+  6: "text-base",
+};
+
 export default function Heading(props: HeadingProps) {
   const Tag = `h${props.level}` as keyof JSX.IntrinsicElements;
-  return <Tag className="text-xl font-bold">{props.children}</Tag>;
+  return (
+    <Tag className={`${sizeMap[props.level]} font-bold`}>{props.children}</Tag>
+  );
 }
