a8e9f93a10ee5460b9f8800c1f1d4e9d612027d9
vincentowler <vincentowler@outlook.com>
2022-09-27 10:54:08 -0400

    Add onChange

--- src/shared/Input.tsx
+++ src/shared/Input.tsx
@@ -1,5 +1,6 @@
 type InputProps = {
   className?: string;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
   id: string;
   label: string;
   type?: "text" | "number" | "email" | "password" | "tel";
@@ -12,6 +13,7 @@ export default function Input({
   label,
   type = "text",
   value,
+  onChange,
 }: InputProps) {
   return (
     <div className={className}>
@@ -20,6 +22,7 @@ export default function Input({
       </label>
       <input
         value={value}
+        onChange={onChange}
         id={id}
         className="border border-gray-600 p-2"
         type={type}
