16be4880c3e4c6e30948eb074ca753c622c0c3b8
vincentowler <vincentowler@outlook.com>
2022-09-27 15:20:53 -0400

    Add error

---
+++ src/shared/Error.tsx
@@ -0,0 +1,11 @@
+type ErrorProps = {
+  error: string;
+};
+
+export default function Error({ error }: ErrorProps) {
+  return (
+    <p role="alert" className="text-red-600 font-bold">
+      {error}
+    </p>
+  );
+}
