f2ccf9b870e2c40bbb81a102aba2caed2a64e5dc
vincentowler <vincentowler@outlook.com>
2022-09-27 12:37:19 -0400

    Centralized change handler

--- src/Admin.tsx
+++ src/Admin.tsx
@@ -1,17 +1,46 @@
-import { foodTags } from "./food";
+import { useState } from "react";
+import { Food, foodTags } from "./food";
 import Button from "./shared/Button";
 import Checkbox from "./shared/Checkbox";
 import CheckboxList from "./shared/CheckboxList";
 import Heading from "./shared/Heading";
 import Input from "./shared/Input";
 
+const emptyFood: Food = {
+  name: "",
+  image: "",
+  price: 0,
+  description: "",
+  tags: [],
+};
+
 export default function Admin() {
+  const [food, setFood] = useState(emptyFood);
+
+  function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const { id, value } = event.target;
+    // React injects the current state value when a function is passed to setState
+    setFood((currentFood) => ({ ...currentFood, [id]: value }));
+  }
+
   return (
     <>
       <Heading level={2}>Admin</Heading>
       <form>
-        <Input id="name" label="Name" className="my-4" />
-        <Input id="description" label="Description" className="my-4" />
+        <Input
+          id="name"
+          label="Name"
+          className="my-4"
+          onChange={handleInputChange}
+          value={food.name}
+        />
+        <Input
+          id="description"
+          label="Description"
+          className="my-4"
+          onChange={handleInputChange}
+          value={food.description}
+        />
         <Input id="price" label="Price" type="number" className="my-4" />
         <Input id="image" label="Image filename" className="my-4" />
         <CheckboxList label="Tags">
