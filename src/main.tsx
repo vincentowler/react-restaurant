5e0dfb19ff5f19549d88dafc1f69259a0b3edcc5
vincentowler <vincentowler@outlook.com>
2022-09-27 14:36:54 -0400

    Add toast

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { Toaster } from "react-hot-toast";
 import "./index.css";
 
 // Warning: StrictMode will render twice in dev only. This can catch subtle bugs.
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
+      <Toaster />
       <App />
     </BrowserRouter>
   </React.StrictMode>
