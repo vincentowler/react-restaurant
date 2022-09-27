112e055ead3a48e551734d881cd68a4bd0ebaf0a
vincentowler <vincentowler@outlook.com>
2022-09-27 14:23:24 -0400

    Implement add

--- src/services/foodsApi.ts
+++ src/services/foodsApi.ts
@@ -6,6 +6,12 @@ export async function getFoods(): Promise<Food[]> {
 }
 
 export async function addFood(food: NewFood): Promise<Food> {
-  const resp = await fetch("http://localhost:3001/foods");
+  const resp = await fetch("http://localhost:3001/foods", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(food),
+  });
   return resp.json();
 }
