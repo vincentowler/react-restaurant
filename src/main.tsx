1948e43d55557b376ddf577d8e16ad7a361ffe75
vincentowler <vincentowler@outlook.com>
2022-09-28 15:46:46 -0400

    Add error boundary on app root

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,16 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { Toaster } from "react-hot-toast";
 import "./index.css";
+import { ErrorBoundary } from "react-error-boundary";
 
 // Warning: StrictMode will render twice in dev only. This can catch subtle bugs.
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Toaster />
-      <App />
-    </BrowserRouter>
+    <ErrorBoundary fallback={<h1>Oops! Sorry, an error occurred.</h1>}>
+      <BrowserRouter>
+        <Toaster />
+        <App />
+      </BrowserRouter>
+    </ErrorBoundary>
   </React.StrictMode>
 );
